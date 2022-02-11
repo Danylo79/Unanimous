3292019e0e6a488a85099abbcf1f577c46c6d564
Danylo79 <dan.komisarenko@gmail.com>
2022-02-11 11:29:00 -0500

    Added ClassManager to server

--- server/src/main/java/dev/dankom/unanimous/UnanimousServer.java
+++ server/src/main/java/dev/dankom/unanimous/UnanimousServer.java
@@ -1,6 +1,13 @@
 package dev.dankom.unanimous;
 
+import dev.dankom.interfaces.impl.ThreadMethodRunner;
+import dev.dankom.interfaces.runner.MethodRunner;
+import dev.dankom.logger.LogManager;
+import dev.dankom.logger.abztract.DefaultLogger;
+import dev.dankom.logger.interfaces.ILogger;
+import dev.dankom.operation.operations.ShutdownOperation;
 import dev.dankom.unanimous.file.FileManager;
+import dev.dankom.unanimous.manager.ClassManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +16,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UnanimousServer {
+    public static final ILogger logger = LogManager.addLogger("unanimous", new DefaultLogger());
+    private static UnanimousServer instance;
+
     private FileManager fileManager;
+    private ClassManager classManager;
 
     public UnanimousServer() {
         this.fileManager = new FileManager();
+        this.classManager = new ClassManager(fileManager);
+        instance = this;
+
+        new ShutdownOperation(new ThreadMethodRunner(() -> {
+            classManager.save();
+        }), "shutdown-save", logger);
     }
 
     public void run(String[] args) {
@@ -21,6 +38,18 @@ public class UnanimousServer {
 
     public static void main(String[] args) {
         new UnanimousServer().run(args);
+    }
+
+    public FileManager getFileManager() {
+        return fileManager;
+    }
+
+    public ClassManager getClassManager() {
+        return classManager;
+    }
+
+    public static UnanimousServer getInstance() {
+        return instance;
     }
 
     @Configuration
